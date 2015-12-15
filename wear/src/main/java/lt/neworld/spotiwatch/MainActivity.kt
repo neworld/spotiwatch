a2be0f8540f03bd87fe6efad63de5baa83eca76b
Andrius Semionovas <andrius.semionovas@vinted.com>
2015-12-15 21:56:59 +0200

    Send message "next" from wear

--- wear/src/main/java/lt/neworld/spotiwatch/MainActivity.kt
+++ wear/src/main/java/lt/neworld/spotiwatch/MainActivity.kt
@@ -2,11 +2,65 @@ package lt.neworld.spotiwatch
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.util.Log
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.wearable.Node
+import com.google.android.gms.wearable.Wearable
+import kotlinx.android.synthetic.main.activity_main.*
+import lt.neworld.spotiwatch.shared.MESSAGE_PATH_NEXT
 
-class MainActivity : WearableActivity() {
+class MainActivity : WearableActivity(), GoogleApiClient.ConnectionCallbacks {
+    private lateinit var googleApiClient: GoogleApiClient
+    private var node: Node? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        connectGoogleApiClient()
+
+        button_next.setOnClickListener() { sendMessage(MESSAGE_PATH_NEXT) }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        googleApiClient.connect()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        googleApiClient.disconnect()
+    }
+
+    private fun connectGoogleApiClient() {
+        googleApiClient = GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .build()
+    }
+
+    override fun onConnected(bundle: Bundle?) {
+        Wearable.NodeApi.getConnectedNodes(googleApiClient).setResultCallback {
+            node = it.nodes.first()
+        }
+    }
+
+    override fun onConnectionSuspended(i: Int) {
+    }
+
+    private fun sendMessage(path: String) {
+        node?.let {
+            Wearable.MessageApi.sendMessage(googleApiClient, it.id, path, null).setResultCallback {
+                if (it.status.isSuccess) {
+                    Log.i(TAG, "Success send message with path: $path")
+                } else {
+                    Log.e(TAG, "Failed send message with status code: ${it.status.statusCode}")
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val TAG = "WearMainActivity"
     }
 }
